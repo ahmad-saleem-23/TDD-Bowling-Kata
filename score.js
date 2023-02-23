fa9bf87b069a39d1a6419e523f18158168eda9ce
Ahmad Saleem <ahmadsaleem94@hotmail.com>
2023-02-23 14:37:43 +1300

    Scoring normal frames

--- score.js
+++ score.js
@@ -1,5 +1,8 @@
 module.exports={scoreFrame, }
 
 function scoreFrame (frame){
-  return frame [0] + frame [1]
+  
+  // return frame [0] + frame [1]
+
+  return frame.reduce((acc, curr) => acc + curr, 0 )
 }
