21d2f8510f33494b745926d560a22c21e36ad70c
Ahmad Saleem <ahmadsaleem94@hotmail.com>
2023-02-23 15:23:48 +1300

    working on test for spare advance

--- score.js
+++ score.js
@@ -2,20 +2,30 @@ module.exports={scoreFrame, }
 
 function scoreFrame (game){
   
-  // return frame [0] + frame [1]
-
- let result = game.map((frame) => {
+ 
+  // we map over the whole game array to check the score of every frame. and we return the frame score.
+ let result = game.map((frame, indx, arr) => {
 
     // variable for a single frame value
     let frameScore = frame [0] + frame [1]
     
+    // we check if the game is a spare
     if (frameScore === 10){
 
-      return frame.reduce((acc, curr) => acc + curr, 0 )
+      // we check the value. we can automaticly do 
+      // let spareFrame = 10 
+
+      let spareFrame = frame.reduce((acc, curr) => acc + curr, 0 )
+
+      spareFrame + arr[indx + 1][0]
+        
+
+      return spareFrame
 
     } else 
     return frame.reduce((acc, curr) => acc + curr, 0 )
   })
 
+  // we reduse the mapped game array to get the final score.
   return result.reduce((acc, curr) => acc + curr, 0 )
 }
--- score.test.js
+++ score.test.js
@@ -21,14 +21,30 @@ describe('the test will do the following',() =>{
   // })
 
   test('scores a normal game', () => {
-    const frame =[ [5, 4], [5, 4]]
-    const expected = 18
+    const frames = [
+        [2, 0], [4, 2], [6, 0], [2, 4], [1, 5], [7, 0], [5, 2], [7, 0], [2, 6], [8, 1]
+      ]
+    const expected = 64 
+    const actual = score.scoreFrame(frames)
+    expect(actual).toBe(expected)
+  })
+
+
+  test('scores a spare game', () => {
+    const frame =[ [5, 5], [5, 4]]
+    const expected = 19
     const actual = score.scoreFrame(frame)
     expect(actual).toBe(expected)
   })
 
 
-
-
+  test('scores a spare game', () => {
+    const frames = [
+  [6, 1], [4, 0], [6, 4], [2, 7], [3, 5], [5, 0], [5, 5], [0, 0], [1, 6], [7, 2]
+]
+    const expected = 71 
+    const actual = score.scoreFrame(frames)
+    expect(actual).toBe(expected)
+  })
   
 })
